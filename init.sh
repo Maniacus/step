a96c77c95ee7703ce491f9f96c440cfaffa0bdeb
Maniacus <maniacus@mail.ru>
2016-03-09 16:25:47 +0300

    goforit

--- init.sh
+++ init.sh
@@ -3,10 +3,9 @@ ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 /etc/init.d/nginx restart
 
 
-ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-gunicorn --workers=2 --bind=0.0.0.0:8080 hello:app
-/etc/init.d/gunicorn restart
 
+
+﻿
 
 
 
