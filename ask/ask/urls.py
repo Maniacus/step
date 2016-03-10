590bb5a3a47310384053b5df68ce247d954124dd
Maniacus <maniacus@mail.ru>
2016-03-10 16:23:54 +0300

    last

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -3,20 +3,20 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('qa.views',
+urlpatterns = patterns [
 
-   url(r'^$', 'test', name='index'),
+   url(r'^$', 'qa.views.test'),
 
-   url(r'^login/', 'test', name='login'),
+   url(r'^login/', 'qa.views.test'),
 
-   url(r'^signup/', 'test', name='signup'),
+   url(r'^signup/', 'qa.views.test'),
 
-   url(r'^question/(?P<id>[0-9]+)/$', 'test', name='question'),
+   url(r'^question/\d+/$', 'qa.views.test'),
 
-   url(r'^ask/', 'test', name='ask'),
+   url(r'^ask/', 'qa.views.test'),
 
-   url(r'^popular/', 'test', name='popular'),   
+   url(r'^popular/', 'qa.views.test'),   
 
-   url(r'^new/', 'test', name='new'),  
+   url(r'^new/', 'qa.views.test'),  
 
-)
+]
