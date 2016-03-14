ab986f88f95d91be14c813f3a1f6dcccdd64ecb2
Maniacus <maniacus@mail.ru>
2016-03-14 09:14:52 +0300

    qa3

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
 
-ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln   -sf   /home/box/web/etc/nginx.conf    /etc/nginx/sites-enabled/test.conf
+
+
+sudo ln   -sf   /home/box/web/etc/gunicorn.ask.conf      /etc/gunicorn.d/ask
+
 /etc/init.d/nginx restart
