1e68f9027b922563ade0a83cc4a1827640ed796d
Maniacus <maniacus@mail.ru>
2016-03-18 08:58:27 +0300

    DB

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -30,8 +30,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
- #   'django.contrib.admin',
- #   'django.contrib.auth',
+    'django.contrib.admin',
+    'django.contrib.auth',
+	'django.contrib.contenttypes',
  #   'django.contrib.contenttypes',
  #   'django.contrib.sessions',
  #   'django.contrib.messages',
@@ -43,6 +44,7 @@ MIDDLEWARE_CLASSES = (
  #   'django.middleware.common.CommonMiddleware',
 #    'django.middleware.csrf.CsrfViewMiddleware',
 #    'django.contrib.auth.middleware.AuthenticationMiddleware',
+	 'django.contrib.auth.models.User' # from TZ
  #   'django.contrib.messages.middleware.MessageMiddleware',
 #    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+class User(models.Model)
+	username = models.CharField(max_length=30)  
+	first_name = models.CharField(max_length=30, blank=True)  
+	last_name = models.CharField(max_length=30, blank=True)  
+	password = models.CharField(max_length=30) 
+
+
+class Question(models.Model)
+	title = models.CharField(max_length=50)    
+	text = models.CharField(max_length=255)    
+	added_at = models.DateTimeField(blank=True)
+	rating =  models.CharField(max_length=50)    
+	author = models.CharField(max_length=255)
+	likes =  models.CharField(max_length=50)    
+	
+class Answer(models.Model)
+	text = models.CharField(max_length=255)    
+	added_at = models.DateTimeField(blank=True)
+	question = models.ManyToManyField(Question)
+	author = models.CharField(max_length=255)
+
 # Create your models here.
